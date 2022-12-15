3b97a6e69e7bfcaadf2a749e4f8358fcf058a781
Екатерина Иванова <ekaterinaivanova@MacBook-Pro-Ekaterina.local>
2022-12-15 19:38:38 +0300

    Add pretty response and multiple news support

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 from GoogleNews import GoogleNews
 import telebot
+import sqlite3 as sl
+
+NEWS_LIMIT = 3
 
 with open('bot_token.txt', 'r') as f:
-	token = f.readline().strip()
+    token = f.readline().strip()
 bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Привет! Чтобы узнать последние новости, введите то о чем вы хотите узнать")
+    bot.reply_to(message, "Привет! Чтобы узнать последние новости, введите то о чем вы хотите узнать")
 
 
-@bot.message_handler(func=lambda message: True)
+@bot.message_handler(commands=['ping'])
+def ping(message):
+    bot.send_message(message.chat.id, "Pong")
+
+
+@bot.message_handler(commands=['google'])
 def echo_all(message):
-	googlenews = GoogleNews(lang='ru')
-	googlenews.get_news(message.text)
-
-	answer = googlenews.results()[0]
-	bot.reply_to(message, answer['title'])
+    googlenews = GoogleNews(lang='ru')
+    googlenews.get_news(message.text)
+    results = googlenews.results()
+    for i in range(NEWS_LIMIT):
+        answer = results[i]
+        bot.send_message(message.chat.id, '*' + answer['title'] + '*', parse_mode='Markdown')
+        bot.send_message(message.chat.id, answer['link'])
 
 
 bot.infinity_polling()
-
